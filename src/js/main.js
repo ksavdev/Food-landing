494f5ae529837d0720aa9e8c4e41d96fe0f5259e
Kostia Savchenko <k.savchenko.dev@gmail.com>
2024-12-23 13:05:38 +0300

    Add show, hide, and fade animations for tab content transitions

--- src/js/main.js
+++ src/js/main.js
@@ -6,15 +6,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
-        });
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');});
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
 
